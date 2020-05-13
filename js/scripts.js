dad6e8ab07291dbf795a1498d3c02ec17d2455e6
Joshua <hosanajoshua@gmail.com>
2020-05-13 06:19:50 +0300

    Added a form in the contact us section and a js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
   $("form#contact-us").submit(function(event){
-    // event.preventDefault();
     var name = $("input#MERGE1").val();
     var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+    var message = $("textarea#message").val();
+    if ($("input#name").val() && $("input#email").val()){
       alert (name + ", we have received your message. Thank you for reaching out to us.");
     }
     else {
       alert("Please enter your name and email!");
     }
+
+    event.preventDefault();
     
   });
 
